104560e791bef1b39fb665cd86f663e12d784be2
goveaangel <goveaangel090@gmail.com>
2025-11-25 19:30:07 -0600

    update frontend cargar_datos

--- pages/1_cargar_datos.py
+++ pages/1_cargar_datos.py
@@ -31,46 +31,27 @@ st.markdown("---")
 df = st.session_state["raw_data"]
 summary = st.session_state["raw_data_summary"]
 
-col1, col2 = st.columns(2)
-
-with col1:
-    st.markdown("### 👀 Vista previa")
-    if df is None:
-        st.info("Aquí se mostrará la vista previa cuando cargues un archivo válido.")
-    else:
-        st.dataframe(df.head())
-
-with col2:
-    st.markdown("### ℹ️ Información rápida")
-
-    if summary is None:
-        st.write("- Número de filas y columnas")
-        st.write("- Tipos de variables")
-        st.write("- Estadísticos básicos (min, max, media, etc.)")
+st.markdown('### 👀 Vista previa')
+if df is None:
+    st.info("Aquí se mostrará la vista previa cuando cargues un archivo válido.")
+else:
+    st.dataframe(df.head())
     
-    else:
-        col2_1, col2_2 = st.columns(2)
-
-        with col2_1:
-            st.write(f"**Filas:** {summary['n_rows']}")
-            st.write(f"**Columnas:** {summary['n_cols']}")
-
-        with col2_2:
-            st.write("**Tipos de datos:**")
-            st.dataframe(
-                summary["dtypes"].to_frame("dtype"),
-                height=200,
-                use_container_width=True
-            )
-
-st.markdown("---")
+    st.write(f"**Filas:** {summary['n_rows']}")
+    st.write(f"**Columnas:** {summary['n_cols']}")
 
 # Bloques extra con más detalle
 if df is not None and summary is not None:
+    st.markdown("---")
+
+    with st.expander('🔢 Tipos de datos'):
+         st.dataframe(
+            summary["dtypes"].to_frame("dtype"),
+            height=200,
+            use_container_width=True
+        )
     with st.expander("🔍 Valores faltantes por columna"):
         st.write(summary["na_counts"])
 
     with st.expander("📊 Resumen estadístico"):
         st.write(summary["describe"])
-else:
-    st.info("Sube un archivo para ver el detalle de valores faltantes y estadísticas.")
